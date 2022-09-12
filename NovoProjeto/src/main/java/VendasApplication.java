36d6e3ca1b21e574c81213346a36e25f33886842
Flaviano <flavianoventuras@hotmail.com>
2022-09-12 11:24:16 -0300

    adicionando

---
+++ NovoProjeto/src/main/java/VendasApplication.java
@@ -0,0 +1,11 @@
+public class VendasApplication {
+
+    @springBootApplication  
+    public static   void main() {
+        main(null);
+    }
+
+    @springBootApplication  
+    public static void main(String[] args)
+        SpringApplication.run(VendasApplication.class, args);
+}
